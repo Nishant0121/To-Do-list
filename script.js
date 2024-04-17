78a9ca5c5ecb7b53cdf6b66aa99ac363e692282d
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-04-17 16:41:08 +0530

    edit

--- script.js
+++ script.js
@@ -46,9 +46,19 @@ function CreateHtmlStorage() {
                             <td>${task.description}</td>
                             <td>${task.responsible}</td>
                             <td>${task.eta}</td>
-                            <td "><i class="fa-solid fa-pen-to-square"><input type="checkbox" onchange="ToggleCompletion(${index})" ${
+                            <td class=" d-flex align-items-center"><div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" id="completed-${index}" onchange="ToggleCompletion(${index})" ${
         task.completed ? "checked" : ""
-      }></i></td>
+      }>
+        <label class="form-check-label" for="completed-${index}"></label>
+    </div><button
+        type="button"
+        class="btn btn-danger px-2 py-1 "
+        onclick="DeleteTask()"
+      ><i class="fa-solid fa-trash"></i></button>
+      <button type="button" class="btn btn-secondary px-2 py-1" onclick="EditTask(${index})">
+      <i class="fa-solid fa-edit"></i>
+      </button></td>
                         </tr>`;
       $("#tablebody").append(taskHtml);
     });
@@ -69,6 +79,40 @@ function ClearStorage() {
   localStorage.clear();
   location.reload();
 }
-var allEntries = JSON.parse(localStorage.getItem("allEntries")) || [];
 
+function DeleteTask(index) {
+  var allEntries = JSON.parse(localStorage.getItem("allEntries")) || [];
+  allEntries.splice(index, 1);
+  localStorage.setItem("allEntries", JSON.stringify(allEntries));
+  location.reload();
+}
+
+function EditTask(index) {
+  var allEntries = JSON.parse(localStorage.getItem("allEntries")) || [];
+  var task = allEntries[index];
+  $("#description").val(task.description);
+  $("#responsible").val(task.responsible);
+  $("#eta").val(task.eta);
+  $("#save").attr("onclick", `SaveEditedTask(${index})`);
+  $("#exampleModal").modal("show");
+}
+
+function SaveEditedTask(index) {
+  var allEntries = JSON.parse(localStorage.getItem("allEntries")) || [];
+  var formData = $("#task").serializeArray();
+  var editedTask = {};
+
+  $.each(formData, function (index, entry) {
+    editedTask[entry.name] = entry.value;
+  });
+
+  editedTask.completed = allEntries[index].completed;
+  allEntries[index] = editedTask;
+
+  localStorage.setItem("allEntries", JSON.stringify(allEntries));
+  $("#exampleModal").modal("hide");
+  location.reload();
+}
+
+var allEntries = JSON.parse(localStorage.getItem("allEntries")) || [];
 console.log(allEntries);
